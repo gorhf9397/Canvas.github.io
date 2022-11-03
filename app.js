d567ddb803e34935111b11b673fd033247efd7ea
seokgeun <trr456@naver.com>
2022-11-03 21:52:01 +0900

    context 사용 fillRect사용하여 좌표 설정

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("하이");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+//css랑 같은 크기를 줘야함
+ctx.fillRect(50,50,100,200)
+//fillRect 4가지 속성이 존재(x:number, y:number, w:number, h: number)
