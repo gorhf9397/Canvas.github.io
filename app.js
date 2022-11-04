a24ba6956536e275d265ee5a0d63bf7834d29d93
gorhf9397 <trr456@naver.com>
2022-11-05 01:05:12 +0900

    context.arc사용 사람 그리기

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ canvas.width = 800;
 canvas.height = 800;
 //css랑 같은 크기를 줘야함
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325,100);
-ctx.lineTo(450,200);
+ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+// context.arc 원 만들기
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+//      x,y,반지름,startangle,endangle(n*Math.PI 에 따라 원의 둘레설정)
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
