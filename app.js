6b7a5f16f994c1189161071f55616c75ec7f29bb
gorhf9397 <trr456@naver.com>
2022-11-05 01:59:39 +0900

    event활용 x,y좌표사용

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ canvas.width = 800;
 canvas.height = 800;
 //css랑 같은 크기를 줘야함
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
-// context.arc 원 만들기
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-//      x,y,반지름,startangle,endangle(n*Math.PI 에 따라 원의 둘레설정)
-ctx.fill();
+ctx.lineWidth = 2;
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+]
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+function onClick(e) {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
