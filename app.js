59cb6a5c972881c44c782ddc3eeceeaaca7e243f
gorhf9397 <trr456@naver.com>
2022-11-07 16:29:45 +0900

    채우기,그리기 기능 추가 및 수정

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.querySelector("#fill-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.querySelector("#color");
 const lineWidth = document.querySelector("#line-width");
@@ -8,6 +9,7 @@ canvas.height = 800;
 //css랑 같은 크기를 줘야함
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(e) {
     if (isPainting) {
@@ -43,12 +45,28 @@ function onColorClick(e) {
     ctx.fillStyle = colorValue;
     color.value = colorValue;
 }
+function onModeClick(e) {
+    //console.log(e);
+    if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "그리기";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "채우기";
+    }
+}
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, 800, 800);
+    }
+}
 
 //그리기 부분
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 //input이벤트부분
 lineWidth.addEventListener("change", onLineWidthChange);
@@ -57,3 +75,4 @@ lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 //console.log(colorOptions);
 colorOptions.forEach(con => con.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
